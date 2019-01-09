cb9eb9f31eaef20976d15364e862dd922be6a732
oscar <oscar@2DAM-PROFESOR.info.quevedo.local>
2019-01-09 13:03:55 +0100

    Merge origin/master
    Conflicts:
    	BaseDatosPool/src/main/java/main/MainChorraPruebaBD.java

---
+++ ServidorApiItemShop/src/main/java/services/APIServicios.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+/**
+ *
+ * @author user
+ */
+public class APIServicios {
+
+    public int checkAPI(String API) {
+        if ("KK".equals(API)) {
+            return 1;
+        } else {
+            return 1;
+    
+        }
+    }
+}
